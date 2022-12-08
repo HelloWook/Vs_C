2976d796de766c650e47a1e851c55f6b183a4132
남정욱 <113816822+HelloWook@users.noreply.github.com>
2022-12-08 17:20:13 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace FractionApp
 {
-    class Program
+    class Fraction
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        int numerator;                              // 분자 필드
+        int denominator;                            // 분모 필드
+        public Fraction(int num, int denom) {       // 생성자
+            numerator = num;
+            denominator = denom;
+        }
+        public void PrintFraction() {               // 출력 메소드
+            Console.WriteLine(numerator + "/" + denominator);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Fraction f = new Fraction(1, 2);
+            f.PrintFraction();
         }
     }
 }
